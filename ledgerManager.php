da2976b179ffacfcd122f0ad29986b880cfbc9af
Jarrett <thejart@gmail.com>
2016-01-24 12:13:21 -0500

    implemented some of the common manager methods using PDO

--- ledgerManager.php
+++ ledgerManager.php
@@ -1,59 +1,111 @@
 <?php
 
 class LedgerManager {
-	protected $table = "ledger";
+	protected $pdo = null;
+	protected $table = null;
+	protected $currentBalance = null;
 
-	public function updateTransaction($transactionId)
+	public function __construct($username, $password, $database, $table = 'ledger')
 	{
-		// update $this->table set description = $desc, $amount = $amount, cleared = $cleared
-		// where id = $transactionId
-
-		// there can be a separate clearTransaction() that calls this, if necessary
+		$this->pdo = new PDO("mysql:host=localhost;dbname=". $database, $username, $password);
+		$this->table = $table;
 	}
 
-	public function insertTransaction()
+	public function updateTransaction($transactionId, $description, $amount, $cleared)
 	{
-		// insert into $this->table (description, amount, credit) values
-		// ($desc, $amount, $credit)
+		$query = $this->pdo->prepare("
+			update {$this->table}
+			set description = :description,
+			set amount = :amount,
+			set cleared = :cleared
+			where id = :transactionId
+		");
+		return $query->execute([
+			':description' => $description,
+			':amount' => $amount,
+			':cleared' => $cleared,
+			':transactionId' => $transactionId
+		]);
+	}
+
+	public function insertTransaction($description, $amount, $credit)
+	{
+		$query = $this->pdo->prepare("
+			insert into {$this->table}
+			(description, amount, credit)
+			values (:description, :amount, :credit)
+		");
+		return $query->execute([
+			':description' => $description,
+			':amount' => $amount,
+			':credit' => $credit,
+		]);
 	}
 
 	public function deleteTransaction($transactionId)
 	{
-		// delete from $this->table where id = $transactionId
+		$query = $this->pdo->prepare("
+			delete from {$this->table}
+			where id = :transactionId
+		");
+		return $query->execute([':transactionId' => $transactionId]);
 	}
 
 	/**
 	 * This method returns the sum of all credit or debit transactions
 	 *
 	 * @param bool $credit
-	 * @return int
+	 * @return float
 	 */
 	protected function getTotalSumCreditTransactions($credit = true)
 	{
-		// select count(amount) from $this->table where credit = $credit
+		// TODO: add support for a time range which can be used on the summary page
+		$query = $this->pdo->prepare("
+			select sum(amount) amount
+			from {$this->table}
+			where credit = :credit
+		");
+		$query->execute([':credit' => $credit]);
+		$row = $query->fetch(PDO::FETCH_ASSOC);
+		return (float)$row['amount'];
 	}
 
 	/**
 	 * This method returns an array of the current balance, debit, and credit values
 	 *
-	 * @return int[]
+	 * @return float[]
 	 */
-	public function getBalance()
+	protected function calculateBalance()
 	{
 		$totalCredits = $this->getTotalSumCreditTransactions(true);
 		$totalDebits = $this->getTotalSumCreditTransactions(false);
-		return [ $totalCredits-$totalDebits, $totalDebits, $totalCredits ];
+		$this->currentBalance = $totalCredits - $totalDebits;
+	}
+
+	public function getCurrentBalance()
+	{
+		if (!isset($this->currentBalance)) {
+			$this->calculateBalance();
+		}
+		return $this->currentBalance;
 	}
 
 	public function getUnclearedAmount()
 	{
-		// select sum(amount) from $this->table where credit = false and cleared = false
+		$query = $this->pdo->prepare("
+			select sum(amount) amount
+			from {$this->table}
+			where credit = false and cleared = false
+		");
+		$row = $query->fetch(PDO::FETCH_ASSOC);
+		return (float)$row['amount'];
 	}
 
 	protected function getNextPayday()
 	{
-		// TODO: figure out the best way to do this.  right now i'm parsing the output of `cal`
-		// ...so stop doing that
+		// TODO: 
+		// if today <= 15 the return 15th of month
+		// else select last_day(now())
 	}
 
 	public function numberOfDaysLeftInPayPeriod()
@@ -61,17 +113,39 @@ class LedgerManager {
 		// use the results of $this->getNextPayday() to figure this out
 	}
 
-	public function retrieveARangeOfTransactions($startDate, $endDate = 'now')
+	public function retrieveARangeOfTransactions($startDate, $endDate = 'now()', $runningBalance = null)
 	{
-		/**
-		 *
-		 * runningBalance can be calculated on the fly, per row (runningBalance-previousAmount)
-		 *
-		 * return = [
-		 *	[ description, amount, runningBalance ]
-		 * ]
-		 *
-		 */
+		// TODO: finalize support for last two parmas.  this will be handy when requesting
+		// more transactions from the past
+		$allTransactions = [];
+		if (!$runningBalance) {
+			$runningBalance = $this->getCurrentBalance();
+		}
+
+		$query = $this->pdo->prepare("
+			select id, credit, description, amount, time, cleared
+			from {$this->table}
+			where time >= :startDate
+			and time <= :endDate
+			order by time desc
+		");
+		$query->execute([
+			":startDate" => $startDate,
+			":endDate" => $endDate
+		]);
+
+		$rows = $query->fetchAll(PDO::FETCH_ASSOC);
+		foreach ($rows as $row) {
+			$row['balance'] = $runningBalance;
+			$allTransactions[] = $row;
+			// Realize that we're calculating balances backwards in time
+			if ($row['credit'] == 1) {
+				$runningBalance -= $row['amount'];
+			} else {
+				$runningBalance += $row['amount'];
+			}
+		}
+		return $allTransactions;
 	}
 
 	public function getAllUnclearedTransactionsOutsideCurrentWindow($startDate)
